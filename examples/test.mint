885a46bf827f997667bcc38ce42211198b450743
darilrodriguez.2@gmail.com <darilrodriguez.2@gmail.com>
2023-05-24 00:49:26 -0300

    add comparation overload to float

--- examples/test.mint
+++ examples/test.mint
@@ -1,8 +1,3 @@
-let w: List = [2f, 3f, 1f, 2f] 
-let inputs: List = [0f, 1f]
+import module as math
 
-let bias: float = 1.5f
-
-let calculate: float = 0f + 2f
-
-print(calculate)
+print("max:", math.max(1f, 2f))
--- src/mfloat.cpp
+++ src/mfloat.cpp
@@ -2,6 +2,7 @@
 #include "mint.h"
 #include "mlist.h"
 #include "mstr.h"
+#include "mbool.h"
 
 mType* mFloat::Type = new mType(
     "float",
@@ -14,6 +15,19 @@ mType* mFloat::Type = new mType(
         mFloat::Type->methods["mSub"] = new mFunction(&mFloat::mSub);
         mFloat::Type->methods["mMul"] = new mFunction(&mFloat::mMul);
         mFloat::Type->methods["mDiv"] = new mFunction(&mFloat::mDiv);
+
+        mFloat::Type->methods["mEq"] = new mFunction(&mFloat::mEq);
+        mFloat::Type->methods["mNe"] = new mFunction(&mFloat::mNe);
+        mFloat::Type->methods["mLt"] = new mFunction(&mFloat::mLt);
+        mFloat::Type->methods["mGt"] = new mFunction(&mFloat::mGt);
+
+        mFloat::Type->methods["mNeg"] = new mFunction(&mFloat::mNeg);
+        mFloat::Type->methods["mPos"] = new mFunction(&mFloat::mPos);
+        mFloat::Type->methods["mNot"] = new mFunction(&mFloat::mNot);
+        mFloat::Type->methods["mPreInc"] = new mFunction(&mFloat::mPreInc);
+        mFloat::Type->methods["mPreDec"] = new mFunction(&mFloat::mPreDec);
+        mFloat::Type->methods["mPostInc"] = new mFunction(&mFloat::mPostInc);
+        mFloat::Type->methods["mPostDec"] = new mFunction(&mFloat::mPostDec);
     },
     []() -> mObject* {
         return new mFloat();
@@ -110,4 +124,118 @@ mObject *mFloat::mDiv(mObject *_args, mObject *_kwargs, mObject *_self) {
     }
 
     return nullptr;
+}
+
+mObject *mFloat::mEq(mObject *_args, mObject *_kwargs, mObject *_self) {
+    const mList* args = (mList*)_args;
+
+    mFloat* self = (mFloat*)_self;
+
+    mObject* right = args->GetItem(0);
+
+    if (right->type == mInt::Type) {
+        return self->value == ((mInt*)right)->value ? mBool::True : mBool::False;
+    }
+
+    if (right->type == mFloat::Type) {
+        return self->value == ((mFloat*)right)->value ? mBool::True : mBool::False;
+    }
+
+    return nullptr;
+}
+
+mObject *mFloat::mNe(mObject *_args, mObject *_kwargs, mObject *_self) {
+    const mList* args = (mList*)_args;
+
+    mFloat* self = (mFloat*)_self;
+
+    mObject* right = args->GetItem(0);
+
+    if (right->type == mInt::Type) {
+        return self->value != ((mInt*)right)->value ? mBool::True : mBool::False;
+    }
+
+    if (right->type == mFloat::Type) {
+        return self->value != ((mFloat*)right)->value ? mBool::True : mBool::False;
+    }
+
+    return nullptr;
+}
+
+mObject *mFloat::mLt(mObject *_args, mObject *_kwargs, mObject *_self) {
+    const mList* args = (mList*)_args;
+
+    mFloat* self = (mFloat*)_self;
+
+    mObject* right = args->GetItem(0);
+
+    if (right->type == mInt::Type) {
+        return self->value < ((mInt*)right)->value ? mBool::True : mBool::False;
+    }
+
+    if (right->type == mFloat::Type) {
+        return self->value < ((mFloat*)right)->value ? mBool::True : mBool::False;
+    }
+
+    return nullptr;
+}
+
+mObject *mFloat::mGt(mObject *_args, mObject *_kwargs, mObject *_self) {
+    const mList* args = (mList*)_args;
+
+    mFloat* self = (mFloat*)_self;
+
+    mObject* right = args->GetItem(0);
+
+    if (right->type == mInt::Type) {
+        return self->value > ((mInt*)right)->value ? mBool::True : mBool::False;
+    }
+
+    if (right->type == mFloat::Type) {
+        return self->value > ((mFloat*)right)->value ? mBool::True : mBool::False;
+    }
+
+    return nullptr;
+}
+
+mObject *mFloat::mNeg(mObject *_args, mObject *_kwargs, mObject *_self) {
+    mFloat* self = (mFloat*)_self;
+
+    return new mFloat(-self->value);
+}
+
+mObject *mFloat::mPos(mObject *_args, mObject *_kwargs, mObject *_self) {
+    mFloat* self = (mFloat*)_self;
+
+    return new mFloat(+self->value);
+}
+
+mObject *mFloat::mNot(mObject *_args, mObject *_kwargs, mObject *_self) {
+    mFloat* self = (mFloat*)_self;
+
+    return new mFloat(!self->value);
+}
+
+mObject *mFloat::mPreInc(mObject *_args, mObject *_kwargs, mObject *_self) {
+    mFloat* self = (mFloat*)_self;
+
+    return new mFloat(++self->value);
+}
+
+mObject *mFloat::mPreDec(mObject *_args, mObject *_kwargs, mObject *_self) {
+    mFloat* self = (mFloat*)_self;
+
+    return new mFloat(--self->value);
+}
+
+mObject *mFloat::mPostInc(mObject *_args, mObject *_kwargs, mObject *_self) {
+    mFloat* self = (mFloat*)_self;
+
+    return new mFloat(self->value++);
+}
+
+mObject *mFloat::mPostDec(mObject *_args, mObject *_kwargs, mObject *_self) {
+    mFloat* self = (mFloat*)_self;
+
+    return new mFloat(self->value--);
 }
